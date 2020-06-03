86de55fba641a0cc60299dba685fe66373f69d32
ilyin0 <ilyindeveloper@gmail.com>
2020-06-03 21:06:44 +0300

    added clear_tables, create_data_table, create_table_view, insert_data_into_tables, insert_into_data_table

---
+++ upgrade/test/insert_into_data_table.sql
@@ -0,0 +1,64 @@
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1996, 'Кирилл Петрович', 1964, 'Игорь Ковалевич', 'БАТЭ', 'Неман', 'Goal', 'Дубайич', 'БАТЭ', 'Нападающий', 12, '03-May-2020 18:00', 'Borisov', 'Борисов-Арена', 13121, 15, 6580);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1996, 'Кирилл Петрович', 1964, 'Игорь Ковалевич', 'БАТЭ', 'Неман', 'Goal', 'Стасевич', 'БАТЭ', 'Полузащитник', 17, '03-May-2020 18:00', 'Borisov', 'Борисов-Арена', 13121, 15, 6580);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1996, 'Кирилл Петрович', 1964, 'Игорь Ковалевич', 'БАТЭ', 'Неман', 'Goal', 'Нехайчик', 'БАТЭ', 'Полузащитник', 70, '03-May-2020 18:00', 'Borisov', 'Борисов-Арена', 13121, 15, 6580);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1996, 'Кирилл Петрович', 1964, 'Игорь Ковалевич', 'БАТЭ', 'Неман', 'Goal', 'Кадимян', 'Неман', 'Полузащитник', 31, '03-May-2020 18:00', 'Borisov', 'Борисов-Арена', 13121, 15, 6580);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1996, 'Кирилл Петрович', 1964, 'Игорь Ковалевич', 'БАТЭ', 'Неман', 'Yellow card', 'Дубайич', 'БАТЭ', 'Нападающий', 22, '03-May-2020 18:00', 'Borisov', 'Борисов-Арена', 13121, 15, 6580);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1996, 'Кирилл Петрович', 1964, 'Игорь Ковалевич', 'БАТЭ', 'Неман', 'Yellow card', 'Забелин', 'Неман', 'Полузащитник', 23, '03-May-2020 18:00', 'Borisov', 'Борисов-Арена', 13121, 15, 6580);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1996, 'Кирилл Петрович', 1964, 'Игорь Ковалевич', 'БАТЭ', 'Неман', 'Yellow card', 'Коваль', 'Неман', 'Нападающий', 64, '03-May-2020 18:00', 'Borisov', 'Борисов-Арена', 13121, 15, 6580);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1996, 'Кирилл Петрович', 1964, 'Игорь Ковалевич', 'БАТЭ', 'Неман', 'Yellow card', 'Лешко', 'Неман', 'Защитник', 72, '03-May-2020 18:00', 'Borisov', 'Борисов-Арена', 13121, 15, 6580);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1964, 'Игорь Ковалевич', 2016, 'Александр Седнев', 'Неман', 'Рух Брест', 'Goal', 'Кадимян', 'Неман', 'Полузащитник', 68, '09-May-2020 17:00', 'Grodno', 'ЦСК Неман', 8479, 16, 5460);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1964, 'Игорь Ковалевич', 2016, 'Александр Седнев', 'Неман', 'Рух Брест', 'Goal', 'Кадимян', 'Неман', 'Полузащитник', 91, '09-May-2020 17:00', 'Grodno', 'ЦСК Неман', 8479, 16, 5460);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1964, 'Игорь Ковалевич', 2016, 'Александр Седнев', 'Неман', 'Рух Брест', 'Goal', 'Васильев', 'Рух Брест', 'Полузащитник', 4, '09-May-2020 17:00', 'Grodno', 'ЦСК Неман', 8479, 16, 5460);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1964, 'Игорь Ковалевич', 2016, 'Александр Седнев', 'Неман', 'Рух Брест', 'Goal', 'Гречихо', 'Рух Брест', 'Полузащитник', 15, '09-May-2020 17:00', 'Grodno', 'ЦСК Неман', 8479, 16, 5460);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1964, 'Игорь Ковалевич', 2016, 'Александр Седнев', 'Неман', 'Рух Брест', 'Goal', 'Седько', 'Рух Брест', 'Полузащитник', 55, '09-May-2020 17:00', 'Grodno', 'ЦСК Неман', 8479, 16, 5460);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1964, 'Игорь Ковалевич', 2016, 'Александр Седнев', 'Неман', 'Рух Брест', 'Goal', 'Андроник', 'Рух Брест', 'Полузащитник', 90, '09-May-2020 17:00', 'Grodno', 'ЦСК Неман', 8479, 16, 5460);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1964, 'Игорь Ковалевич', 2016, 'Александр Седнев', 'Неман', 'Рух Брест', 'Yellow card', 'Лешко', 'Неман', 'Защитник', 68, '09-May-2020 17:00', 'Grodno', 'ЦСК Неман', 8479, 16, 5460);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1964, 'Игорь Ковалевич', 2016, 'Александр Седнев', 'Неман', 'Рух Брест', 'Yellow card', 'Легчилин', 'Неман', 'Полузащитник', 87, '09-May-2020 17:00', 'Grodno', 'ЦСК Неман', 8479, 16, 5460);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1964, 'Игорь Ковалевич', 2016, 'Александр Седнев', 'Неман', 'Рух Брест', 'Yellow card', 'Гайдучик', 'Рух Брест', 'Защитник', 35, '09-May-2020 17:00', 'Grodno', 'ЦСК Неман', 8479, 16, 5460);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1964, 'Игорь Ковалевич', 2016, 'Александр Седнев', 'Неман', 'Рух Брест', 'Yellow card', 'Васильев', 'Рух Брест', 'Полузащитник', 51, '09-May-2020 17:00', 'Grodno', 'ЦСК Неман', 8479, 16, 5460);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1964, 'Игорь Ковалевич', 2016, 'Александр Седнев', 'Неман', 'Рух Брест', 'Yellow card', 'Васильев', 'Рух Брест', 'Полузащитник', 86, '09-May-2020 17:00', 'Grodno', 'ЦСК Неман', 8479, 16, 5460);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1964, 'Игорь Ковалевич', 2016, 'Александр Седнев', 'Неман', 'Рух Брест', 'Yellow card', 'Андроник', 'Рух Брест', 'Полузащитник', 81, '09-May-2020 17:00', 'Grodno', 'ЦСК Неман', 8479, 16, 5460);
+
+INSERT INTO DATA_TABLE (CHAMPIONSHIP, LEAGUE, FOUNDED_TEAM_1, TRAINER_TEAM_1, FOUNDED_TEAM_2, TRAINER_TEAM_2, TEAM_1, TEAM_2, EVENT, PLAYER_LAST_NAME, PLAYER_TEAM, POSITION, EVENT_TIME, DATE_TIME, LOCATION, VENUE, CAPACITY, AVG_PRICE, TICKETS_SOLD) 
+VALUES (2020, 'Belarusian Premier League', 1964, 'Игорь Ковалевич', 2016, 'Александр Седнев', 'Неман', 'Рух Брест', 'Red card', 'Васильев', 'Рух Брест', 'Полузащитник', 82, '09-May-2020 17:00', 'Grodno', 'ЦСК Неман', 8479, 16, 5460);
+
+COMMIT;
