1cc209e794bfc2690e8cfd229c6afacb4ed5078a
ahomtsov <dandyforwork@gmail.com>
2020-06-03 17:39:07 +0300

    fixed drop and install

--- drop/main.sql
+++ drop/main.sql
@@ -1,18 +1,18 @@
 SET TIME ON
 SET TIMING ON
 
-PROMPT Enter username
 DEFINE USER_NAME = &&1
 
 SPOOL DROP_SCHEMA.LOG
 
 SET SERVEROUTPUT ON
-PROMPT Username calling to drop_user: &&USER_NAME
+
+PROMPT Name of user to be dropped: &&USER_NAME
 PROMPT
 
 @drop_user.sql &&USER_NAME
 
-PROMPT Username caling to drop_tablspace: '&&USER_NAME._DATA'
+PROMPT Name of tablespace to be dropped: '&&USER_NAME._DATA'
 PROMPT
 
 @drop_tablespace.sql &&USER_NAME
